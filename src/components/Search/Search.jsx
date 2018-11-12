b24e44e887e967480852658ab2c72a016bce05b2
William Peirce <bill@seeyourcampsite.com>
2018-11-11 19:59:17 -0500

    Added one more change

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { Component} from "react";
+import axios from "axios";
 import "./Search.css";
 
-function Search(props) {
-  return (
-    <div className="search-field">
-      <p>Search for a Book</p>
-      <div className="input-group mb-3">
-        <input type="text" placeholder="Enter title keywords"/>
-        <div className="input-group-append">
-          <button className="btn btn-primary" type="button"><i className="fas fa-search"></i></button>
+class Search extends Component {
+  render() {
+    return (
+      <div className="search-field">
+        <p>Search for a Book</p>
+        <div className="input-group mb-3">
+          <input type="text" placeholder="Enter title keywords"/>
+          <div className="input-group-append">
+            <button className="btn btn-primary" type="button"><i className="fas fa-search"></i></button>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Search;
